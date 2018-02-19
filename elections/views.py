05d829991fc20d90a1c5e5492a2ac55d4e0ccc78
YouHuiDong <youheedong@gmail.com>
2018-02-19 20:26:54 +0900

    views.py에 index함수 수정, Candidat테이블 정보 표시

--- elections/views.py
+++ elections/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Candidate
 def index(request):
-    return HttpResponse("Hello world")
+    candidates = Candidate.objects.all()
+    str = "" #마지막에 리턴해줄 문자열
+    for candidate in candidates:
+        str += "{name}기호 {num}번 ({area})<BR>".format(name=candidate.name, num=candidate.party_number, area=candidate.area)
+        str += candidate.introduction + "<P>"
+    return HttpResponse(str)
