ec68568ea66f592ee9444b5b975f0b014353bdb0
YouHuiDong <youheedong@gmail.com>
2018-02-19 20:46:29 +0900

    template추가 index.html정적으로 생성

--- elections/views.py
+++ elections/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Candidate
+
+'''
 def index(request):
     candidates = Candidate.objects.all()
     str = "" #마지막에 리턴해줄 문자열
@@ -8,3 +11,7 @@ def index(request):
         str += "{name}기호 {num}번 ({area})<BR>".format(name=candidate.name, num=candidate.party_number, area=candidate.area)
         str += candidate.introduction + "<P>"
     return HttpResponse(str)
+'''
+def index(request):
+    candidates = Candidate.objects.all()
+    return render(request, 'elections/index.html', {'candidates':candidates})
