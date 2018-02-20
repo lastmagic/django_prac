0512835247b93c5402f18c5747ac3c880d35a456
YouHuiDong <youheedong@gmail.com>
2018-02-20 14:09:52 +0900

    area.html choice function add, using form

--- elections/views.py
+++ elections/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Candidate
+from .models import Candidate, Poll, Choice
+import datetime
 
 '''
 def index(request):
@@ -18,4 +19,12 @@ def index(request):
     return render(request, 'elections/index.html', context)
 
 def areas(request, area):
-    return HttpResponse(area)
+    today = datetime.datetime.now()
+    try:
+        poll = Poll.objects.get(area=area, start_date__lte = today, end_date__gte=today)
+        candidates = Candidate.objects.filter(area = area)
+    except:
+        poll = None
+        candidates = None
+    context = {'candidates': candidates, 'area':area, 'poll':poll}
+    return render(request, 'elections/area.html', context)
